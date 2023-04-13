6073c08abd93655d99200bc05f25b175473034b4
Phong Lam <phonglam@Phongs-Laptop.attlocal.net>
2023-04-13 12:24:53 -0700

    update Epoch stat

--- apps/explorer/src/pages/epochs/utils.ts
+++ apps/explorer/src/pages/epochs/utils.ts
@@ -19,9 +19,35 @@ export function useEpochProgress(suffix: string = 'left') {
             ? Math.min(((Date.now() - start) / (end - start)) * 100, 100)
             : 0;
 
+    const timeLeftMs = Date.now() - end;
+    const timeLeftMin = Math.floor(timeLeftMs / 60000);
+
+    let label;
+    if (timeLeftMs >= 0) {
+        label = 'Ending soon';
+    } else if (timeLeftMin >= -1) {
+        label = 'About a min left';
+    } else {
+        label = `${time} ${suffix}`;
+    }
+
     return {
         epoch: data?.epoch,
         progress,
-        label: end <= Date.now() ? 'Ending soon' : `${time} ${suffix}`,
+        label,
+    };
+}
+
+export function getElapsedTime(start: number, end: number) {
+    const diff = end - start;
+
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+
+    return {
+        hours,
+        minutes: minutes - hours * 60,
+        seconds: seconds - minutes * 60,
     };
 }
